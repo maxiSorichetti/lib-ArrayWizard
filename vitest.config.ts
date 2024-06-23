3eb32999f82cabb114b47f537bc18117b151891e
Maximiliano Sorichetti <91890016+maxiSorichetti@users.noreply.github.com>
2024-06-23 20:50:31 -0300

    chore: add throttle and helpers globals (#7)
    * chore: add strings
    
    * Chore/add strings, global paths, and throttle (#6)
    
    * chore: add paths aliases
    
    * chore: add throttle and helpers globals

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import { defineConfig, mergeConfig } from 'vitest/config'
 
 import viteConfig from './vite.config'
@@ -6,8 +7,12 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
-      globals: true
+      globals: true,
+      setupFiles: './tests/helpers', // archivo que se ejecuta antes de cada test
       // environment: 'jsdom' // emular tiempos, creamos un entorno virtual no usamos el browser real
+      alias: {
+        '@src': path.resolve(__dirname, '/src')
+      }
     }
   })
 )

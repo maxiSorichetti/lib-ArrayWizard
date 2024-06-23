c9f476b678adc2dfacd856ab3a1cc2fd5e42153b
maxiSorichetti <maxisorichetti19@gmail.com>
2024-06-23 20:45:33 -0300

    chore: add throttle and helpers globals

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,7 @@ export default mergeConfig(
   defineConfig({
     test: {
       globals: true,
+      setupFiles: './tests/helpers', // archivo que se ejecuta antes de cada test
       // environment: 'jsdom' // emular tiempos, creamos un entorno virtual no usamos el browser real
       alias: {
         '@src': path.resolve(__dirname, '/src')

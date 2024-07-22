2e8373e121f263e0304a780fcc8ebe7c70d5bb17
maxiSorichetti <maxisorichetti19@gmail.com>
2024-07-22 00:40:29 -0300

    feat: add groupBy fn

--- src/arrays/index.ts
+++ src/arrays/index.ts
@@ -1,6 +1,7 @@
 export * from './arraylable'
 export * from './difference'
 export * from './flattering'
+export * from './groupby'
 export * from './intersection'
 export * from './map'
 export * from './sum'
